414906f5f5528b9e14cebb1f2b6005e1dae8a9fa
BrianLevin <bml201095@gmail.com>
2021-04-21 16:33:10 -0400

    rendered the type of day

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res){
@@ -15,18 +15,22 @@ app.get("/", function(req, res){
 var today = new Date();
 var currentDay =  today.getDay();
 
-var day = "",
+var day = "";
+
 if (today.getDay() == 6 || today.getDay() == 0 ){
     day = "Weekend";
 
-    res.render("list", {kindOfDay: day});
+    
 
-res.write(" <h1>It's the weekend!</h1>");
+
 } else{
     day= "Weekday"
-res.sendFile(__dirname + "/index.html")
+
+
 
 }
+
+res.render("list", {kindOfDay: day});
 
 });
 
