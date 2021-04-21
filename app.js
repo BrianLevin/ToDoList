dbcb69b86c30b9a0ce213518cf8581f4cf084298
BrianLevin <bml201095@gmail.com>
2021-04-21 16:25:33 -0400

    passing variables in the lsits file

--- app.js
+++ app.js
@@ -15,10 +15,15 @@ app.get("/", function(req, res){
 var today = new Date();
 var currentDay =  today.getDay();
 
+var day = "",
 if (today.getDay() == 6 || today.getDay() == 0 ){
+    day = "Weekend";
+
+    res.render("list", {kindOfDay: day});
 
 res.write(" <h1>It's the weekend!</h1>");
 } else{
+    day= "Weekday"
 res.sendFile(__dirname + "/index.html")
 
 }
