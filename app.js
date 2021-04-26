d8a69392e296d8e5014fa6ad7a54800e37c852ee
BrianLevin <bml201095@gmail.com>
2021-04-26 16:31:20 -0400

    added the body parcer which will grab the value of the post method

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
@@ -27,44 +28,15 @@ var today = new Date();
  };
 
   var day = today.toLocaleDateString("en-US", options );
-// var currentDay =  today.getDay();
-
-// var day = "";
-
-// switch (currentDay){
-// case 0:
-// day = "Sunday";
-// break;
-// case 1:
-// day = "Monday";
-// break;
-// case 2:
-// day = "Tuesday";
-// break;
-// case 3:
-// day = "Wednesday";
-// break;
-
-// case 4:
-// day = "Thursday";
-// break;
-// case 5:
-// day = "Friday";
-// break;
-// case 6:
-// day = "Saturday";
-// break;
-
-// default:
-//     console.log("Error: current day is equal to"+ currentDay);
 
 
-// }
 
 res.render("list", {
 kindOfDay: day
 });
 });
+
+
 
 app.post("/", function(req, res){
 
